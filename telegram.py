4a048bde48d5831865690ed39cef4cc3c629c3bb
portatil <eugenio90@gmail.com>
2017-06-14 17:50:43 +0200

    Fix issue, now it wont ask again for channel's data without admin privilege

--- telegram.py
+++ telegram.py
@@ -26,7 +26,7 @@ def chat_info(name,timeout=10):
             print("NOOOOOO, it was impossible to get the info of {}...".format(name))
             return {}
         return sender.chat_info(name)
-    except Exception:
+    except Exception as err:
         print("Wooops, chat_info failed. Just {} more times...".format(timeout))
         time.sleep(1)
         return chat_info(name,timeout-1)
@@ -38,10 +38,15 @@ def channel_get_members(name,timeout=5):
             print("Maybe it's because YOU HAVE NO POWER HERE. Tsss. Evil.")
             return []
         return sender.channel_get_members(name,LIMIT)
-    except Exception:
+    except Exception as err:
         print("Wooops, channel_get_members failed. Just {} more times...".format(timeout))
+        potorro = err.args
         time.sleep(1)
-        return channel_get_members(name,timeout-1)
+        if "CHAT_ADMIN_REQUIRED" in potorro[0]:
+            print("YOU HAVE NO POWER HERE. Tsss. Evil. NotAnAdmin")
+            return []
+        else:
+            return channel_get_members(name,timeout-1)
 
 
 j_dialog = json.loads(json.dumps(dialog_list()))
